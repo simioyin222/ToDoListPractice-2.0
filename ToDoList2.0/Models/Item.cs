a9abc5b8309c76ed7ca9cfacffe00310b67c2261
simioyin222 <simi.oyinkolade@gmail.com>
2024-02-06 14:24:18 -0600

    gives more options to item model and supporting categories and tags with validation and data

--- ToDoList2.0/Models/Item.cs
+++ ToDoList2.0/Models/Item.cs
@@ -1,20 +1,38 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ToDoList.Models
 {
     public class Item
     {
         public int ItemId { get; set; }
-
-        [Required(ErrorMessage = "The item's description can't be empty!")]
+        
+        [Required(ErrorMessage = "Please enter a description.")]
         public string Description { get; set; }
+        
+        [Display(Name = "Completed")]
+        public bool IsCompleted { get; set; } = false; // Default to false for new items
+        
+        [DataType(DataType.Date)]
+        [Display(Name = "Due Date")]
+        public DateTime DueDate { get; set; }
 
-        [Range(1, int.MaxValue, ErrorMessage = "You must add your item to a category. Have you created a category yet?")]
+        // Assuming a foreign key relationship with a Category
+        [Display(Name = "Category")]
         public int CategoryId { get; set; }
+        
+        // Navigation property for the related Category
+        public virtual Category Category { get; set; }
+        
+        // For a many-to-many relationship with Tags
+        public virtual ICollection<ItemTag> JoinEntities { get; set; }
 
-        public Category Category { get; set; }
-        public List<ItemTag> JoinEntities { get; }
+        // Constructor to initialize the collection property
+        public Item()
+        {
+            this.JoinEntities = new HashSet<ItemTag>();
+        }
     }
 }
