786525962198e73a494cbfc40a6fc92f3986845d
simioyin222 <simi.oyinkolade@gmail.com>
2024-02-06 15:00:14 -0600

    final migrations

---
+++ ToDoList2.0/Migrations/20240206205719_UpdateCategoriesTable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList2._0.Migrations
+{
+    public partial class UpdateCategoriesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Categories",
+                newName: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "Categories",
+                newName: "Id");
+        }
+    }
+}
