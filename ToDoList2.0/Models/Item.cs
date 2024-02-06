c560ba6a89c2948c94c9e2bf989a0591615ec9d1
simioyin222 <simi.oyinkolade@gmail.com>
2024-02-06 13:31:19 -0600

    add two new properties to item calass for due dates

--- ToDoList2.0/Models/Item.cs
+++ ToDoList2.0/Models/Item.cs
@@ -7,6 +7,8 @@ namespace ToDoList.Models
     {
         public int ItemId { get; set; }
         public string Description { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public DateTime DueDate { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
         public List<ItemTag> JoinEntities { get; set; }
